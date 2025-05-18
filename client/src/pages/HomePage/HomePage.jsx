95a06a642ee5a2995b388fcee08f6d9606b327dd
Sang121 <20010797@st.phenikaa-uni.edu.vn>
2025-05-18 17:19:22 +0700

    edit front end, update props cho ListProduct

--- client/src/pages/HomePage/HomePage.jsx
+++ client/src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import TypeProducts from "../../components/typeProducts/typeProducts";
+import Sidebar from "../../components/SideBar/SideBar";
 import BelowHeader from "../../components/BelowHeader/BelowHeader";
 import styles from "./HomePage.module.css";
 import Carousel from "../../components/Carousel/Carousel";
@@ -7,15 +7,15 @@ import ImgAds from "../../components/imgAds/ImgAds";
 import ListProduct from "../../components/ListProducts/ListProduct";
 import FilterPrice from "../../components/FilterPrice/FilterPrice";
 function HomePage() {
+
   return (
     <div>
-      <BelowHeader />
       <main>
         <div className={styles["home-page-container"]}>
           <div className={styles["home-page"]}>
             <div className={styles["home-page-sidebar"]}>
-            <TypeProducts />
-            <FilterPrice />
+              <Sidebar />
+              <FilterPrice />
             </div>
             <div className={styles["home-page-contain"]}>
               <div className="carousel-container">
@@ -23,19 +23,18 @@ function HomePage() {
               </div>
               <div className={styles["home-page-list"]}>
                 <h2>Thời trang nam</h2>
-                <ListProduct />
+                <ListProduct Collections={"mens-shirts"} />
               </div>
               <ImgAds img="https://1557691689.e.cdneverest.net/fast/2826x0/filters:format(webp)/static.5sfashion.vn/storage/home/collect/iW0O0Erw6eTGQwt2ASd9OUyyvz9GRMY6.png" />
               <div className={styles["home-page-list"]}>
                 <h2> Thời Trang Nữ</h2>
-                <ListProduct />
+                <ListProduct Collections={"womens-dresses"} />
               </div>
               <ImgAds img="https://1557691689.e.cdneverest.net/fast/2826x0/filters:format(webp)/static.5sfashion.vn/storage/home/collect/iW0O0Erw6eTGQwt2ASd9OUyyvz9GRMY6.png" />
               <div className={styles["home-page-list"]}>
                 <h2> Phụ Kiện </h2>
-                <ListProduct />
+                <ListProduct Collections={"tops"} />
               </div>
-              
             </div>
           </div>
         </div>
