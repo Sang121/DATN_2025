4e0e3984817fec6bacc28df8882ee89b7b26d645
Sang121 <20010797@st.phenikaa-uni.edu.vn>
2025-05-18 17:23:29 +0700

    add thêm trang hiện thị sản phẩm theo danh mục

---
+++ client/src/pages/TypeProductPage/TypeProductPage.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import Sidebar from "../../components/SideBar/SideBar";
+import FilterPrice from "../../components/FilterPrice/FilterPrice";
+import styles from "./TypeProductPage.module.css";
+import ListProduct from "../../components/ListProducts/ListProduct";
+import { useParams } from "react-router-dom";
+
+function TypeProductPage() {
+  const { type } = useParams();
+
+  return (
+    <div className={styles["type-product-page-container"]}>
+      <div className={styles["type-product-page"]}>
+        <div className={styles.SideBar}>
+          <Sidebar />
+          <FilterPrice />
+        </div>
+        <div className={styles["type-product-page-content"]}>
+          <div className={styles["type-product-page-list"]}>
+            <div className={styles["product-detail-breadcrumb"]}>
+              <h1>
+                <a href="/">Trang chủ</a> / {type}
+              </h1>
+            </div>
+
+            <ListProduct Collections={type} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default TypeProductPage;
