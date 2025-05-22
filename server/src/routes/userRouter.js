e3799a442a5fc6c9fcfa92047972cc967fe8f60f
Sang121 <20010797@st.phenikaa-uni.edu.vn>
2025-05-23 02:21:45 +0700

    Refactor user router to organize routes and ensure proper middleware usage

--- server/src/routes/userRouter.js
+++ server/src/routes/userRouter.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/UserController');
+const authMiddleware= require('../middleware/authMiddleware')
 
 
-
-router.post('/',userController.createUser);
-
+router.post('/signin',userController.loginUser);
+router.post('/signup',userController.createUser);
+router.put('/update-user/:id',userController.updateUser);
+router.get('/delete-user/:id',authMiddleware,userController.deleteUser);
+router.get('/getAllUser',authMiddleware,userController.getAllUser);
+router.get('/getDetailUser/:id',userController.getDetailUser);
 
 module.exports = router;
 
