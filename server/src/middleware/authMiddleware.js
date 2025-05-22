cc7820f1be8f32b333c9c1b8c398039b215f1192
Sang121 <20010797@st.phenikaa-uni.edu.vn>
2025-05-23 02:21:18 +0700

    Add authentication middleware for token verification and admin access control

---
+++ server/src/middleware/authMiddleware.js
@@ -0,0 +1,30 @@
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const authMiddleware = (req, res, next) => {
+    try {
+        const token = req.headers.token && req.headers.token.split(' ')[1];
+        if (!token) {
+            return res.status(401).json({ status: 'Error', message: 'No token provided' });
+            
+        }
+        jwt.verify(token, process.env.ACCESS_TOKEN, (err, user) => {
+            
+            if(user.payload.isAdmin){
+                req.user = user;
+                next();
+            }
+            else{
+                return res.status(403).json({ status: 'Error', message: 'The authentication' });
+
+            }
+            
+        });
+    } catch (error) {
+        return res.status(500).json({ status: 'Error', message: 'Server error:', error});
+       
+    }
+};
+
+module.exports = authMiddleware;
