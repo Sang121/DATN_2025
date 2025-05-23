b99e48f2c80e0b1a98eaed43cbda1736bbfd83a2
Sang121 <20010797@st.phenikaa-uni.edu.vn>
2025-05-24 03:14:25 +0700

    Add authUserMiddleware for JWT authentication and error handling

---
+++ server/src/middleware/authUserMiddleware.js
@@ -0,0 +1,33 @@
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+dotenv.config();
+const authUserMiddleware = (req, res, next) => {
+    try {
+        const token = req.headers.token && req.headers.token.split(' ')[1];
+        if (!token) {
+
+            return res.status(401).json({ status: 'Error', message: 'No token provided' });
+            
+        }
+        jwt.verify(token, process.env.ACCESS_TOKEN, (err, user) => {
+            if (err) {
+                return res.status(403).json({ status: 'Error', message: 'Token is invalid' });
+            }
+            if(user.isAdmin||user.id==req.params.id){
+
+                req.user = user;
+                next();
+            }
+            else{
+
+                return res.status(403).json({ status: 'Error', message: 'The authenticationn' });
+
+            }
+            
+        });
+    } catch (error) {
+        return res.status(500).json({ status: 'Error', message: 'Server eerror:', error});
+       
+    }
+};
+module.exports = authUserMiddleware;
