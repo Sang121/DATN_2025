13436a7238cbad2e07af62bf53978750b22972b0
Sang121 <20010797@st.phenikaa-uni.edu.vn>
2025-05-27 18:13:26 +0700

    Refactor ListProduct component to use React Query

--- client/src/components/ListProducts/ListProduct.jsx
+++ client/src/components/ListProducts/ListProduct.jsx
@@ -1,32 +1,67 @@
 import React from "react";
 import ProductCard from "../ProductCard/ProductCard";
 import styles from "./ListProduct.module.css";
-import { Flex } from "antd";
-import { useEffect } from "react";
-import { useState } from "react";
-import { BaseUrl } from "../dumpApi";
-function ListProduct({ Collections }) {
-  const [products, setProducts] = useState([]);
-  useEffect(() => {
-         fetch(`https://dummyjson.com/products/category/${Collections}`)
-           .then((res) => res.json())
-           .then((data) => setProducts(data.products));
-      }, [Collections]);
+import { Flex, Spin, Alert } from "antd"; // Import Spin và Alert từ antd
+import { useQuery } from "@tanstack/react-query";
+import { fetchProducts } from '/src/components/fetchProducts.jsx' 
+function ListProduct({ category }) {
+
+   const {
+    data: products,   
+    isLoading,        
+    isError,    
+    error,          
+  } = useQuery({
+    queryKey: ['products', category], 
+    queryFn: () => fetchProducts(category), 
+    staleTime: 1000 * 60 * 5, 
+  });
+  if (isLoading) { 
+    return (
+      <div>
+        <div className={styles.statusContainer}>
+        <Spin size="large" /><p>Loading products...</p>
+        </div>
+      </div>
+    );
+  };
+  if (isError) {
+    return (
+      <div className={styles.statusContainer}>
+        {/* Error.message lấy thông báo từ đối tượng lỗi */}
+        <Alert
+          message="Error"
+          description={
+            error?.message || "Failed to load products. Please try again later."
+          }
+          type="error"
+          showIcon
+        />
+      </div>
+    );
+  }
+
+  if (!products || products.length === 0) {
+    return (
+      <div className={styles.statusContainer}>
+        <p>No products found for this category. Please try a different one.</p>
+      </div>
+    );
+  }
 
   return (
     <div>
       <Flex wrap gap="small" className={styles["list-product-container"]}>
         {products.map((product) => (
           <ProductCard
-            productId={product.id}
+            key={product._id} 
+            productId={product._id}
             image={product.thumbnail}
-            name={product.title}
+            name={product.name}
             price={product.price}
             oldPrice={product.oldPrice}
             rating={product.rating}
-            discount={product.discountPercentage}
-         
-           
+            discount={product.discount}
             extra={product.description}
           />
         ))}
