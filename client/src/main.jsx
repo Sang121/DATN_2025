2eac24bc4d138e48ec96c026ae1903d8edf3f780
Sang121 <20010797@st.phenikaa-uni.edu.vn>
2025-05-27 18:16:57 +0700

    Refactor main.jsx to integrate React Query for improved data management and add React Query Devtools

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,30 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import store from './redux/store.js'
-import { Provider } from 'react-redux'
-createRoot(document.getElementById('root')).render(
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+
+import store from "./redux/store.js";
+import { Provider } from "react-redux";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"; 
+
+
+import "@ant-design/v5-patch-for-react-19";
+
+import { App as AntdApp } from "antd";
+
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-     <Provider store={store}>
-      <App />
-    </Provider>
-  </StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <AntdApp>
+        <Provider store={store}>
+          <App /> 
+        </Provider>
+      </AntdApp>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  </StrictMode>
+);
