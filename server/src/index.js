2acafffd3caf4be3272929c4bb3c569f61ac16be
Sang121 <20010797@st.phenikaa-uni.edu.vn>
2025-05-28 17:00:41 +0700

    Refactor index.js để gửi nhận refresh token qua cookies

--- server/src/index.js
+++ server/src/index.js
@@ -1,22 +1,27 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
 dotenv.config();
-mongoose.set('strictQuery', false);
-const cors = require('cors');
-const connectDB = require('./config/connect');
-const routes = require('./routes');
-const bodyParser = require('body-parser');
+mongoose.set("strictQuery", false);
+const cors = require("cors");
+const connectDB = require("./config/connect");
+const routes = require("./routes");
+const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 
-const app= express();
+const app = express();
 const port = process.env.PORT || 3001;
 
- // Thêm dòng này
- app.use(express.json()); // để xử lý req.body
+// Thêm dòng này
+app.use(express.json()); // để xử lý req.body
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
@@ -25,5 +30,5 @@ connectDB();
 routes(app);
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
