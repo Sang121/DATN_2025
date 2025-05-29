e3ad6f63e6e58ed087550f7c7d6c497c34beb7a2
Sang121 <20010797@st.phenikaa-uni.edu.vn>
2025-05-30 02:14:43 +0700

    cập nhật cấu trúc mô hình người dùng: sửa đổi tên trường access_token và refresh_token, thêm trường fullName và gender

--- server/src/models/userModel.js
+++ server/src/models/userModel.js
@@ -4,15 +4,17 @@ const Schema = mongoose.Schema;
 
 const modelUser = new Schema(
   {
+    fullName: { type: String, require: true },
     username: { type: String, require: true },
     email: { type: String, require: true },
     password: { type: String, require: true },
     phone: { type: String, require: true },
     address: { type: String, require: true },
     isAdmin: { type: Boolean, default: false },
-    accessToken: { type: String, default: null },
-    refreshToken: { type: String, default: null },
+    access_token: { type: String, default: null },
+    refresh_token: { type: String, default: null },
     avatar: { type: String, default: null },
+    gender: { type: String, enum: ["male", "female", "other"] },
     typeLogin: { type: String, enum: ["email", "google"] },
   },
   {
