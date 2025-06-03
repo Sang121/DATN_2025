2da6f407672d1d5575e34e92fd10ae6e46152de6
Sang121 <20010797@st.phenikaa-uni.edu.vn>
2025-06-04 01:46:20 +0700

    check isAdmin

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,40 @@
-import React, { Fragment } from 'react'
-import { Button, Flex } from 'antd';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { routes } from './Routes/index.js'
-import Header from './components/Header/Header.jsx';
-import DefauPage from './pages/DefautPage/DefauPage.jsx';
+import React, { Fragment } from "react";
+import { Button, Flex } from "antd";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { routes } from "./Routes/index.js";
+import Header from "./components/Header/Header.jsx";
+import DefauPage from "./pages/DefautPage/DefauPage.jsx";
+import { useSelector } from "react-redux";
 
- function App() {
-  
+function App() {
+  const user = useSelector((state) => state.user);
   return (
     <div>
-     
       <Router>
         <Routes>
           {routes.map((route, index) => {
-           const Page= route.page
-           const Layout=route.isShowHeader ? DefauPage: Fragment
-           return (
-            <Route key={index} path={route.path} element={
-            <Layout>
-            <Page/>
-             </Layout>
-          } />
-          )
- })}
+            const Page = route.page;
+            const checkAuth = !route.isPrivate || user.isAdmin;
+            const Layout = route.isShowHeader ? DefauPage : Fragment;
+
+            return (
+              route.path &&
+              checkAuth && (
+                <Route
+                  key={index}
+                  path={route.path}
+                  element={
+                    <Layout>
+                      <Page />
+                    </Layout>
+                  }
+                />
+              )
+            );
+          })}
         </Routes>
       </Router>
-
-      
-      </div>
-    
-  )
+    </div>
+  );
 }
-export default App
+export default App;
