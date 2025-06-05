f12123efd12b4893baf197d5c0589e0a4bae8a75
Sang121 <20010797@st.phenikaa-uni.edu.vn>
2025-06-06 04:23:45 +0700

    sửa api search

--- client/src/pages/search/SearchPage.jsx
+++ client/src/pages/search/SearchPage.jsx
@@ -1,12 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Sidebar from "../../components/SideBar/SideBar";
 import FilterPrice from "../../components/FilterPrice/FilterPrice";
 import styles from "./SearchPage.module.css";
 import ListProduct from "../../components/ListProducts/ListProduct";
 import { useParams } from "react-router-dom";
+import { searchProduct } from "../../services/productService";
+import { useQuery } from "@tanstack/react-query";
+import { Flex } from "antd";
+import ProductCard from "../../components/ProductCard/ProductCard";
 
 function SearchPage() {
-  const { query  } = useParams();
+  const { query } = useParams();
+  const [searchQuery, setSearchQuery] = useState(query);
+
+  useEffect(() => {
+    setSearchQuery(query);
+  }, [query]);
+  const {
+    data: searchData,
+    isLoading,
+    isError,
+  } = useQuery({
+    queryKey: ["search", searchQuery],
+    queryFn: () => searchProduct(searchQuery),
+  });
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+  if (isError) {
+    return <div>Error: {isError.message}</div>;
+  }
+
   return (
     <div className={styles["type-product-page-container"]}>
       <div className={styles["type-product-page"]}>
@@ -21,7 +45,27 @@ function SearchPage() {
                 <a href="/">Trang chủ</a> / {query}
               </h1>
             </div>
-            <ListProduct category={query} />
+            <div>
+              <Flex
+                wrap
+                gap="small"
+                className={styles["list-product-container"]}
+              >
+                {searchData.data.map((product) => (
+                  <ProductCard
+                    key={product._id}
+                    productId={product._id}
+                    image={product.images[0]}
+                    name={product.name}
+                    price={product.price}
+                    oldPrice={product.oldPrice}
+                    rating={product.rating}
+                    discount={product.discount}
+                    extra={product.description}
+                  />
+                ))}
+              </Flex>
+            </div>
           </div>
         </div>
       </div>
