789e23a9f4c043f97ac4d5a1d06b6feb1a22aba6
Sang121 <20010797@st.phenikaa-uni.edu.vn>
2025-06-06 00:25:47 +0700

    thêm trang admin với chức năng quản lý người dùng và quản lý sản phẩm

--- client/src/pages/AdminPage/AdminPage.jsx
+++ client/src/pages/AdminPage/AdminPage.jsx
@@ -8,51 +8,56 @@ import {
   SettingOutlined,
   UserOutlined,
 } from "@ant-design/icons";
-import { Divider, Menu, Switch } from "antd";
+import styles from "./AdminPage.module.css";
+import { Col, Divider, Menu, Switch } from "antd";
+import UserManager from "../../components/admin/userManager/UserManager";
+import ProductManager from "../../components/admin/productManager/ProductManager";
+
 const items = [
   {
     key: "user",
     icon: <UserOutlined />,
-    label: "Quản lý người dùng",
+    label: "Người dùng",
   },
   {
     key: "product",
     icon: <ProductOutlined />,
-    label: "Quản lý sản phẩm",
+    label: "Sản phẩm",
   },
 ];
 const AdminPage = () => {
-  const [mode, setMode] = useState("inline");
-  const [theme, setTheme] = useState("light");
-  const [selectedKey, setSelectedKey] = useState();
-  const changeMode = (value) => {
-    setMode(value ? "vertical" : "inline");
-  };
-  const changeTheme = (value) => {
-    setTheme(value ? "dark" : "light");
-  };
+  const [selectedKey, setSelectedKey] = useState("user");
+
   const handleOnClick = ({ key }) => {
     setSelectedKey(key);
   };
   return (
     <>
-      <Switch onChange={changeMode} /> Change Mode
-      <Divider type="vertical" />
-      <Switch onChange={changeTheme} /> Change Style
-      <br />
-      <br />
-      <div>
-        <Menu
-          style={{ width: 256 }}
-          defaultSelectedKeys={["1"]}
-          defaultOpenKeys={["sub1"]}
-          onClick={handleOnClick}
-          mode={mode}
-          theme={theme}
-          items={items}
-        />
-        <div> {selectedKey === "user" && <span>User nè </span>} </div>
-        <div> {selectedKey === "product" && <span>Product nè </span>} </div>
+      <div className={styles["adminContainer"]}>
+        <Col className={styles["mainContent"]}>
+          <Col xs={6} sm={6} md={6} lg={4}>
+            <Menu
+              mode="inline"
+              className={styles["sideBar"]}
+              onClick={handleOnClick}
+              theme="light"
+              items={items}
+            />
+          </Col>
+          <Col xs={18} sm={18} md={18} lg={20} className={styles["content"]}>
+            {selectedKey === "user" ? (
+              <span>
+                <UserManager />
+              </span>
+            ) : (
+              <span><ProductManager/></span>
+            )}
+          </Col>
+          {/* <div className={styles["content"]}>
+            {" "}
+            {selectedKey === "product" && <span>Product nè </span>}{" "}
+          </div> */}
+        </Col>
       </div>
     </>
   );
