8bf9e6121d5f52c6e99ef5213905e4077bf7acdb
Sang121 <20010797@st.phenikaa-uni.edu.vn>
2025-06-17 01:21:15 +0700

    thêm filter tìm kiếm theo tên sản phẩm

--- client/src/pages/AdminPage/component/productManager/ProductManager.jsx
+++ client/src/pages/AdminPage/component/productManager/ProductManager.jsx
@@ -1,8 +1,19 @@
 import classNames from "classnames/bind";
 import styles from "./ProductManager.module.css";
-import { useState } from "react";
-import { message, Table, Button, Space, Spin, Alert, Pagination } from "antd";
-import { DeleteOutlined, PlusOutlined } from "@ant-design/icons";
+import { useRef, useState } from "react";
+import {
+  message,
+  Table,
+  Button,
+  Space,
+  Spin,
+  Alert,
+  Pagination,
+  Input,
+} from "antd";
+import Highlighter from "react-highlight-words";
+
+import { DeleteOutlined, PlusOutlined, SearchOutlined } from "@ant-design/icons";
 import AddProduct from "./AddProduct/AddProduct";
 import {
   deleteProduct,
@@ -18,7 +29,9 @@ function ProductManager() {
   const [limit] = useState(8);
   const [selectedProduct, setSelectedProduct] = useState(null);
   const queryClient = useQueryClient();
-
+  const [searchText, setSearchText] = useState("");
+  const [searchedColumn, setSearchedColumn] = useState("");
+  const searchInput = useRef(null);
   const deleteProductMutation = useMutation({
     mutationFn: (id) => deleteProduct(id),
     onSuccess: () => {
@@ -78,6 +91,104 @@ function ProductManager() {
   const handleDeleteProduct = (id) => {
     deleteProductMutation.mutate(id);
   };
+  const handleSearch = (selectedKeys, confirm, dataIndex) => {
+    confirm();
+    setSearchText(selectedKeys[0]);
+    setSearchedColumn(dataIndex);
+  };
+  const handleReset = (clearFilters) => {
+    clearFilters();
+    setSearchText("");
+  };
+  const getColumnSearchProps = (dataIndex) => ({
+    filterDropdown: ({
+      setSelectedKeys,
+      selectedKeys,
+      confirm,
+      clearFilters,
+      close,
+    }) => (
+      <div style={{ padding: 8 }} onKeyDown={(e) => e.stopPropagation()}>
+        <Input
+          ref={searchInput}
+          placeholder={`Search ${dataIndex}`}
+          value={selectedKeys[0]}
+          onChange={(e) =>
+            setSelectedKeys(e.target.value ? [e.target.value] : [])
+          }
+          onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
+          style={{ marginBottom: 8, display: "block" }}
+        />
+        <Space>
+          <Button
+            type="primary"
+            onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
+            icon={<SearchOutlined />}
+            size="small"
+            style={{ width: 90 }}
+          >
+            Search
+          </Button>
+          <Button
+            onClick={() => clearFilters && handleReset(clearFilters)}
+            size="small"
+            style={{ width: 90 }}
+          >
+            Reset
+          </Button>
+          <Button
+            type="link"
+            size="small"
+            onClick={() => {
+              confirm({ closeDropdown: false });
+              setSearchText(selectedKeys[0]);
+              setSearchedColumn(dataIndex);
+            }}
+          >
+            Filter
+          </Button>
+          <Button
+            type="link"
+            size="small"
+            onClick={() => {
+              close();
+            }}
+          >
+            close
+          </Button>
+        </Space>
+      </div>
+    ),
+    filterIcon: (filtered) => (
+      <SearchOutlined style={{ color: filtered ? "#1677ff" : undefined }} />
+    ),
+    onFilter: (value, record) =>
+      record[dataIndex].toString().toLowerCase().includes(value.toLowerCase()),
+    filterDropdownProps: {
+      onOpenChange(open) {
+        if (open) {
+          setTimeout(() => {
+            var _a;
+            return (_a = searchInput.current) === null || _a === void 0
+              ? void 0
+              : _a.select();
+          }, 100);
+        }
+      },
+    },
+    render: (text) =>
+      searchedColumn === dataIndex ? (
+        <Highlighter
+          highlightStyle={{ backgroundColor: "#ffc069", padding: 0 }}
+          searchWords={[searchText]}
+          autoEscape
+          textToHighlight={text ? text.toString() : ""}
+        />
+      ) : (
+        text
+      ),
+  });
+
 
   const columns = [
     {
@@ -111,19 +222,26 @@ function ProductManager() {
       key: "name",
       width: 200,
       ellipsis: true,
+      ...getColumnSearchProps("name"),
     },
     {
       title: "Giá",
       dataIndex: "price",
       key: "price",
       width: 150,
-      render: (price) => `${price.toLocaleString()} VNĐ`,
+      render: (price) => `$${Intl.NumberFormat('vi-VN', {
+    style: 'currency',
+    currency: 'VND'
+  }).format(price)}`,
+      sorter: (a, b) => a.price - b.price,
+      ...getColumnSearchProps("price"),
     },
     {
       title: "Số lượng còn lại",
-      dataIndex: "stock",
-      key: "stock",
+      dataIndex: "totalStock",
+      key: "totalStock",
       width: 150,
+      sorter: (a, b) => a.price - b.price,
     },
     {
       title: "Action",
@@ -146,6 +264,7 @@ function ProductManager() {
       ),
     },
   ];
+
 
   return (
     <div className={cx("wrapper")}>
