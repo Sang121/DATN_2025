a76c2dea3bedaf8ef9bc179fc4f1ee4b8a066473
Sang121 <20010797@st.phenikaa-uni.edu.vn>
2025-06-18 00:00:11 +0700

    Thêm orderReducer vào cấu hình store để quản lý đơn hàng

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -2,12 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./slices/userSlice";
 import { loadState, saveState } from "../utils/storage";
 import throttle from "lodash/throttle"; 
-
+import orderReducer from "./slices/orderSlice";
 const preloadedState = loadState();
 
 const Store = configureStore({
   reducer: {
     user: userReducer,
+    order: orderReducer,
   },
   preloadedState: preloadedState,
 });
