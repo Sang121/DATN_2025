d488828814a802ce408b25f6cb31db19a7317f73
Sang121 <20010797@st.phenikaa-uni.edu.vn>
2025-06-18 17:34:25 +0700

    thêm trang checkout

--- client/src/Routes/index.js
+++ client/src/Routes/index.js
@@ -1,6 +1,5 @@
 import HomePage from "../pages/HomePage/HomePage";
 import NotFoundPage from "../pages/NotFoundPage/NotFoundPage";
-import OrderPage from "../pages/OrderPage/OrderPage";
 import ProductPage from "../pages/productPage/ProductPage";
 import ProductDetail from "../pages/ProductDetails/ProductDetail";
 import TypeProductPage from "../pages/TypeProductPage/TypeProductPage";
@@ -9,7 +8,9 @@ import SignUpPage from "../pages/SignUpPage/SignUpPage";
 import SearchPage from "../pages/search/SearchPage";
 import ProfilePage from "../pages/profile/profilePage";
 import AdminPage from "../pages/AdminPage/AdminPage";
-import CartPage from "../pages/cartPage/cartPage";
+import CartPage from "../pages/CartPage/CartPage";
+import PaymentPage from "../pages/PaymentPage/PaymentPage";
+import PaymentSuccessPage from "../pages/PaymentSuccessPage/PaymentSuccessPage";
 export const routes = [
   {
     path: "/",
@@ -17,12 +18,12 @@ export const routes = [
     isShowHeader: true,
     isPrivate: false,
   },
-  {
-    path: "/order/:id",
-    page: OrderPage,
-    isShowHeader: true,
-    isPrivate: false,
-  },
+  // {
+  //   path: "/order/:id",
+  //   page: OrderPage,
+  //   isShowHeader: true,
+  //   isPrivate: false,
+  // },
   {
     path: "/products",
     page: ProductPage,
@@ -75,6 +76,18 @@ export const routes = [
   {
     path: "/cart",
     page: CartPage,
+    isShowHeader: true,
+    isPrivate: false,
+  },
+  {
+    path: "/payment",
+    page: PaymentPage,
+    isShowHeader: true,
+    isPrivate: false,
+  },
+  {
+    path: "/payment-success",
+    page: PaymentSuccessPage,
     isShowHeader: true,
     isPrivate: false,
   },
