6f44eca73d899dd3d9e473c28fac6e597e1b168c
Sang121 <20010797@st.phenikaa-uni.edu.vn>
2025-06-19 23:53:57 +0700

    Cập nhật logic trang thanh toán thành công: thêm thông báo lỗi khi thông tin đơn hàng không hợp lệ.

--- client/src/pages/PaymentSuccessPage/PaymentSuccessPage.jsx
+++ client/src/pages/PaymentSuccessPage/PaymentSuccessPage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Result } from "antd";
+import { Button, message, Result } from "antd";
 import { useLocation, useNavigate } from "react-router-dom";
 import { CheckCircleFilled, ShoppingOutlined } from "@ant-design/icons";
 import styles from "./PaymentSuccessPage.module.css";
@@ -8,7 +8,11 @@ function PaymentSuccessPage() {
   const location = useLocation();
   const navigate = useNavigate();
   const { orderId, totalAmount } = location.state || {};
-
+  if (!orderId || !totalAmount) {
+    message.error("Thông tin đơn hàng không hợp lệ.");
+    navigate("/");
+    return null;
+  }
   return (
     <div className={styles.container}>
       <div className={styles.content}>
