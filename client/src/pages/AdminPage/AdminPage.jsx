b7b106d8c7f1d8ebc9476fc324e0c32cbbfaff41
Sang121 <20010797@st.phenikaa-uni.edu.vn>
2025-06-24 01:29:43 +0700

    Thêm component OrderManager và cập nhật giao diện quản lý đơn hàng

--- client/src/pages/AdminPage/AdminPage.jsx
+++ client/src/pages/AdminPage/AdminPage.jsx
@@ -7,11 +7,14 @@ import {
   ProductOutlined,
   SettingOutlined,
   UserOutlined,
+  ShoppingCartOutlined, // Thêm icon cho đơn hàng
 } from "@ant-design/icons";
 import styles from "./AdminPage.module.css";
 import { Col, Divider, Menu, Switch } from "antd";
 import UserManager from "./component/userManager/UserManager";
 import ProductManager from "./component/productManager/ProductManager";
+import OrderManager from "./component/orderManager/OrderManager"; // Import component mới
+
 const items = [
   {
     key: "user",
@@ -23,13 +26,33 @@ const items = [
     icon: <ProductOutlined />,
     label: "Sản phẩm",
   },
+  {
+    key: "order", // Thêm key cho đơn hàng
+    icon: <ShoppingCartOutlined />,
+    label: "Đơn hàng",
+  },
 ];
+
 const AdminPage = () => {
   const [selectedKey, setSelectedKey] = useState("user");
 
   const handleOnClick = ({ key }) => {
     setSelectedKey(key);
   };
+
+  const renderContent = () => {
+    switch (selectedKey) {
+      case "user":
+        return <UserManager />;
+      case "product":
+        return <ProductManager />;
+      case "order":
+        return <OrderManager />;
+      default:
+        return <UserManager />;
+    }
+  };
+
   return (
     <>
       <div className={styles["adminContainer"]}>
@@ -41,26 +64,16 @@ const AdminPage = () => {
               onClick={handleOnClick}
               theme="light"
               items={items}
+              selectedKeys={[selectedKey]}
             />
           </Col>
           <Col xs={18} sm={18} md={18} lg={20} className={styles["content"]}>
-            {selectedKey === "user" ? (
-              <span>
-                <UserManager />
-              </span>
-            ) : (
-              <span>
-                <ProductManager />
-              </span>
-            )}
+            {renderContent()}
           </Col>
-          {/* <div className={styles["content"]}>
-            {" "}
-            {selectedKey === "product" && <span>Product nè </span>}{" "}
-          </div> */}
         </Col>
       </div>
     </>
   );
 };
+
 export default AdminPage;
