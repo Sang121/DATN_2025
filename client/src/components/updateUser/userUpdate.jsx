32705e6c32f5def5b0f0dbe1581386d2e2ceade5
Sang121 <20010797@st.phenikaa-uni.edu.vn>
2025-06-24 10:11:42 +0700

    cập nhật lại giao diện

--- client/src/components/updateUser/userUpdate.jsx
+++ client/src/components/updateUser/userUpdate.jsx
@@ -1,16 +1,42 @@
-import { Form, Select } from "antd";
-import React from "react";
-import { Button, Input, Radio, message as antdMessage } from "antd";
+import { Form, Select, Divider, Row, Col } from "antd";
+import React, { useState, useMemo } from "react";
+import {
+  Button,
+  Input,
+  Avatar,
+  Typography,
+  Upload,
+  message as antdMessage,
+} from "antd";
 import { useMutation } from "@tanstack/react-query";
-import { updateUser } from "../../services/userService"; 
-import { useDispatch } from "react-redux"; 
-import { updateUser as updateReduxUser } from "../../redux/slices/userSlice"; 
+import { updateUser } from "../../services/userService";
+import { useDispatch } from "react-redux";
+import { updateUser as updateReduxUser } from "../../redux/slices/userSlice";
+import {
+  UserOutlined,
+  MailOutlined,
+  PhoneOutlined,
+  HomeOutlined,
+  ManOutlined,
+  WomanOutlined,
+  SaveOutlined,
+  UploadOutlined,
+} from "@ant-design/icons";
+import styles from "./userUpdate.module.css";
+
+const { Title, Text } = Typography;
+const { Option } = Select;
 
 function UserUpdate() {
-  const user = JSON.parse(sessionStorage.getItem("userState"))?.user || {};
-  const { Option } = Select;
+  // Sử dụng useMemo để đảm bảo user không thay đổi tham chiếu sau mỗi lần render
+  const user = useMemo(
+    () => JSON.parse(sessionStorage.getItem("userState"))?.user || {},
+    []
+  );
+
   const [form] = Form.useForm();
-  const dispatch = useDispatch(); 
+  const dispatch = useDispatch();
+  const [avatarUrl, setAvatarUrl] = useState(user.avatar || "");
 
   React.useEffect(() => {
     form.setFieldsValue({
@@ -20,29 +46,30 @@ function UserUpdate() {
       phone: user.phone,
       address: user.address,
     });
+    setAvatarUrl(user.avatar || "");
   }, [user, form]);
 
   const updateUserMutation = useMutation({
-    mutationFn: ({ id, userData }) => updateUser(id, userData), 
+    mutationFn: ({ id, userData }) => updateUser(id, userData),
     onSuccess: (data) => {
-      console.log("Cập nhật thông tin tài khoản thành công:", data); 
+      console.log("Cập nhật thông tin tài khoản thành công:", data);
       antdMessage.success("Cập nhật thông tin tài khoản thành công.");
 
-      const newAccessToken = data.access_token; 
-      const updatedUserData = data.data; 
+      const newAccessToken = data.access_token;
+      const updatedUserData = data.data;
 
       const updatedUser = {
-        ...user, 
-        ...updatedUserData, 
-        access_token: newAccessToken, 
+        ...user,
+        ...updatedUserData,
+        access_token: newAccessToken,
       };
 
       sessionStorage.setItem(
         "userState",
         JSON.stringify({ user: updatedUser })
       );
-    
-      dispatch(updateReduxUser(updatedUser)); 
+
+      dispatch(updateReduxUser(updatedUser));
     },
     onError: (error) => {
       console.error(
@@ -55,7 +82,7 @@ function UserUpdate() {
       } else if (error.message === "Network Error") {
         errorMessage = "Lỗi kết nối mạng. Vui lòng kiểm tra internet.";
       }
-      antdMessage.error(errorMessage); 
+      antdMessage.error(errorMessage);
     },
   });
 
@@ -67,77 +94,170 @@ function UserUpdate() {
     });
   };
 
+  // Tạm thời chỉ hiển thị phần tải lên avatar, chưa có xử lý tải lên thực tế
+  const handleAvatarChange = (info) => {
+    if (info.file.status === "done") {
+      antdMessage.success(`${info.file.name} tải lên thành công`);
+      // Trong thực tế, bạn sẽ lấy URL từ phản hồi tải lên
+      // setAvatarUrl(info.file.response.url);
+    } else if (info.file.status === "error") {
+      antdMessage.error(`${info.file.name} tải lên thất bại.`);
+    }
+  };
+
   return (
-    <Form
-      form={form}
-      name="user_update"
-      onFinish={onFinish}
-      labelCol={{ span: 8 }}
-      wrapperCol={{ span: 16 }}
-      layout="horizontal"
-      initialValues={{
-        fullName: user.fullName,
-        gender: user.gender,
-        email: user.email,
-        phone: user.phone,
-        address: user.address,
-      }}
-    >
-      <h2>Cập nhật thông tin tài khoản</h2>
-      <Form.Item
-        label="Họ và tên"
-        name="fullName"
-        rules={[{ required: true, message: "Họ và tên là bắt buộc!" }]}
-      >
-        <Input placeholder="Họ và tên" />
-      </Form.Item>
-      <Form.Item
-        label="Email"
-        name="email"
-        rules={[{ required: true, message: "Email is required!" }]}
-      >
-        <Input placeholder="email" />
-      </Form.Item>
+    <div className={styles.userUpdateContainer}>
+      <Title level={2} className={styles.formTitle}>
+        <UserOutlined className={styles.titleIcon} /> Thông tin tài khoản
+      </Title>
 
-      <Form.Item
-        name="gender"
-        label="Gender"
-        rules={[{ required: true, message: "Please select gender!" }]}
-      >
-        <Select placeholder="select your gender">
-          <Option value="male">Male</Option>
-          <Option value="female">Female</Option>
-          <Option value="other">Other</Option>
-        </Select>
-      </Form.Item>
-
-      <Form.Item
-        label="Số điện thoại"
-        name="phone"
-        rules={[{ required: true, message: "Số điện thoại là bắt buộc" }]}
-      >
-        <Input placeholder="Số điện thoại" />
-      </Form.Item>
-
-      <Form.Item
-        label="Địa chỉ"
-        name="address"
-        rules={[{ required: true, message: "Địa chỉ là bắt buộc" }]}
-      >
-        <Input placeholder="Địa chỉ" />
-      </Form.Item>
-      <Form.Item style={{display:'flex', justifyContent:'center' }} >
-        <Button
-          type="primary"
-          htmlType="submit"
-          block
-          style={{ width: "auto", }} 
-          loading={updateUserMutation.isPending}
+      {/* <div className={styles.userAvatar}>
+        <Avatar
+          size={100}
+          icon={<UserOutlined />}
+          src={avatarUrl}
+          className={styles.avatarImage}
+        />
+        <Upload
+          name="avatar"
+          showUploadList={false}
+          action="https://run.mocky.io/v3/435e224c-44fb-4773-9faf-380c5e6a2188"
+          onChange={handleAvatarChange}
+          className={styles.avatarUploader}
         >
-          Cập nhật thông tin
-        </Button>
-      </Form.Item>
-    </Form>
+          <Button icon={<UploadOutlined />} className={styles.uploadButton}>
+            Cập nhật ảnh đại diện
+          </Button>
+        </Upload>
+      </div> */}
+
+      <Form
+        form={form}
+        name="user_update"
+        onFinish={onFinish}
+        layout="vertical"
+        className={styles.updateForm}
+        initialValues={{
+          fullName: user.fullName,
+          gender: user.gender,
+          email: user.email,
+          phone: user.phone,
+          address: user.address,
+        }}
+      >
+        <Divider orientation="left" className={styles.sectionDivider}>
+          Thông tin cá nhân
+        </Divider>
+
+        <Row gutter={24}>
+          <Col xs={24} sm={12}>
+            <Form.Item
+              label="Họ và tên"
+              name="fullName"
+              rules={[{ required: true, message: "Họ và tên là bắt buộc!" }]}
+            >
+              <Input
+                prefix={<UserOutlined className={styles.inputIcon} />}
+                placeholder="Nhập họ và tên của bạn"
+                size="large"
+              />
+            </Form.Item>
+          </Col>
+
+          <Col xs={24} sm={12}>
+            <Form.Item
+              name="gender"
+              label="Giới tính"
+              rules={[{ required: true, message: "Vui lòng chọn giới tính!" }]}
+            >
+              <Select
+                placeholder="Chọn giới tính"
+                size="large"
+                className={styles.selectField}
+              >
+                <Option value="male">
+                  <ManOutlined /> Nam
+                </Option>
+                <Option value="female">
+                  <WomanOutlined /> Nữ
+                </Option>
+                <Option value="other">Khác</Option>
+              </Select>
+            </Form.Item>
+          </Col>
+        </Row>
+
+        <Divider orientation="left" className={styles.sectionDivider}>
+          Thông tin liên hệ
+        </Divider>
+
+        <Row gutter={24}>
+          <Col xs={24} sm={12}>
+            <Form.Item
+              label="Email"
+              name="email"
+              rules={[
+                { required: true, message: "Email là bắt buộc!" },
+                { type: "email", message: "Email không hợp lệ!" },
+              ]}
+            >
+              <Input
+                prefix={<MailOutlined className={styles.inputIcon} />}
+                placeholder="Email của bạn"
+                size="large"
+              />
+            </Form.Item>
+          </Col>
+
+          <Col xs={24} sm={12}>
+            <Form.Item
+              label="Số điện thoại"
+              name="phone"
+              rules={[
+                { required: true, message: "Số điện thoại là bắt buộc" },
+                {
+                  pattern: /^[0-9]{10,11}$/,
+                  message: "Số điện thoại không hợp lệ!",
+                },
+              ]}
+            >
+              <Input
+                prefix={<PhoneOutlined className={styles.inputIcon} />}
+                placeholder="Số điện thoại của bạn"
+                size="large"
+              />
+            </Form.Item>
+          </Col>
+        </Row>
+
+        <Form.Item
+          label="Địa chỉ"
+          name="address"
+          rules={[{ required: true, message: "Địa chỉ là bắt buộc" }]}
+        >
+          <Input.TextArea
+            placeholder="Địa chỉ của bạn"
+            size="large"
+            autoSize={{ minRows: 2, maxRows: 6 }}
+            className={styles.textareaField}
+            prefix={<HomeOutlined className={styles.inputIcon} />}
+          />
+        </Form.Item>
+
+        <div className={styles.formActions}>
+          <Button
+            type="primary"
+            htmlType="submit"
+            icon={<SaveOutlined />}
+            className={styles.submitButton}
+            loading={updateUserMutation.isPending}
+            size="large"
+          >
+            Cập nhật thông tin
+          </Button>
+        </div>
+      </Form>
+    </div>
   );
 }
 
