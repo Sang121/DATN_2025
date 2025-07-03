9390b7b81ad7842b68e980958ee63b74aab3c418
Sang121 <20010797@st.phenikaa-uni.edu.vn>
2025-07-03 21:57:43 +0700

    thay đổi slider

--- client/src/components/Carousel/Carousel.jsx
+++ client/src/components/Carousel/Carousel.jsx
@@ -1,82 +1,102 @@
-import React from "react";
-import { Carousel as AntCarousel } from "antd";
+import React, { useRef } from "react";
+import { Carousel as AntCarousel, Button } from "antd";
+import { LeftOutlined, RightOutlined } from "@ant-design/icons";
 import { Link } from "react-router-dom";
 import styles from "./Carousel.module.css";
+import slide1 from "../../assets/img/carousel/1.webp";
+import slide2 from "../../assets/img/carousel/2.webp";
+import slide3 from "../../assets/img/carousel/3.webp";
+import slide4 from "../../assets/img/carousel/4.webp";
 
 function Carousel() {
+  const carouselRef = useRef(null);
+
   // Slides data
   const slides = [
     {
       id: 1,
-      images: [
-        {
-          src: "https://salt.tikicdn.com/cache/w750/ts/tikimsp/1f/7f/6e/78d8c9976eb8b7b3b6133671573d1a56.png",
-          alt: "Thời trang nam",
-          link: "/search/Nam",
-        },
-        {
-          src: "https://salt.tikicdn.com/cache/w750/ts/tikimsp/27/a0/e9/bbe8b3f5cdb8837e352eb891da0a9794.jpg",
-          alt: "Thời trang nữ",
-          link: "/search/Nữ",
-        },
-      ],
+      images: {
+        src: slide1,
+        alt: "Thời trang nam",
+        link: "/search/Nam",
+      },
     },
     {
       id: 2,
-      images: [
-        {
-          src: "https://salt.tikicdn.com/cache/w750/ts/tikimsp/27/a0/e9/bbe8b3f5cdb8837e352eb891da0a9794.jpg",
-          alt: "Phụ kiện thời trang",
-          link: "/search/Phụ%20kiện",
-        },
-        {
-          src: "https://salt.tikicdn.com/cache/w750/ts/tikimsp/1f/7f/6e/78d8c9976eb8b7b3b6133671573d1a56.png",
-          alt: "Giày dép",
-          link: "/search/Giày",
-        },
-      ],
+      images: {
+        src: slide2,
+        alt: "Thời trang nữ",
+        link: "/search/Nữ",
+      },
     },
     {
       id: 3,
-      images: [
-        {
-          src: "https://salt.tikicdn.com/cache/w750/ts/tikimsp/1f/7f/6e/78d8c9976eb8b7b3b6133671573d1a56.png",
-          alt: "Đồng hồ",
-          link: "/search/Đồng%20Hồ",
-        },
-        {
-          src: "https://salt.tikicdn.com/cache/w750/ts/tikimsp/27/a0/e9/bbe8b3f5cdb8837e352eb891da0a9794.jpg",
-          alt: "Túi xách",
-          link: "/search/Túi%20Xách",
-        },
-      ],
+      images: {
+        src: slide3,
+        alt: "Quần",
+        link: "/search/Quần",
+      },
+    },
+    {
+      id: 4,
+      images: {
+        src: slide4,
+        alt: "Áo",
+        link: "/search/Áo",
+      },
     },
   ];
 
+  // Navigation functions
+  const goToPrev = () => {
+    carouselRef.current?.prev();
+  };
+
+  const goToNext = () => {
+    carouselRef.current?.next();
+  };
+
   return (
     <div className={styles.carouselContainer}>
+      <Button
+        className={styles.prevButton}
+        icon={<LeftOutlined />}
+        onClick={goToPrev}
+        shape="circle"
+        size="large"
+        type="primary"
+      />
+
+      <Button
+        className={styles.nextButton}
+        icon={<RightOutlined />}
+        onClick={goToNext}
+        shape="circle"
+        size="large"
+        type="primary"
+      />
+
       <AntCarousel
+        ref={carouselRef}
         autoplay
         effect="fade"
         dots={true}
         className={styles.carousel}
+        autoplaySpeed={4000}
+        speed={800}
       >
         {slides.map((slide) => (
-          <div key={slide.id}>
-            <div className={styles.slider}>
-              {slide.images.map((image, idx) => (
-                <Link to={image.link} key={idx} className={styles.imageLink}>
-                  <img
-                    className={styles.imageSlider}
-                    alt={image.alt}
-                    src={image.src}
-                  />
-                  <div className={styles.imageOverlay}>
-                    <span className={styles.imageText}>{image.alt}</span>
-                  </div>
-                </Link>
-              ))}
-            </div>
+          <div key={slide.id} className={styles.slider}>
+            <Link to={slide.images.link} className={styles.imageLink}>
+              <img
+                className={styles.imageSlider}
+                alt={slide.images.alt}
+                src={slide.images.src}
+              />
+              <div className={styles.imageOverlay}>
+                <span className={styles.imageText}>{slide.images.alt}</span>
+              </div>
+            </Link>
           </div>
         ))}
       </AntCarousel>
